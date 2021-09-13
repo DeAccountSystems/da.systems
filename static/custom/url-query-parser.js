13f8cf504e1d694c0c6997adcebbfa5388cb49fe
longhuicode <longhuicode@foxmail.com>
2021-09-13 19:29:44 +0800

    feat: 添加 DeName 入口；

--- static/custom/url-query-parser.js
+++ static/custom/url-query-parser.js
@@ -12,7 +12,9 @@ let queryString = $.param(getQueryParam())
 
 if (queryString) {
   const gogodasUrl = `https://app.gogodas.com/explorer?${queryString}`
+  const denameUrl = `https://app.dename.com?${queryString}`
   const dasUrl = `https://app.da.systems/explorer?${queryString}`
+  $('#denameUrl').attr('href', denameUrl)
   $('#gogodasUrl').attr('href', gogodasUrl)
   $('#dasUrl').attr('href', dasUrl)
 }
